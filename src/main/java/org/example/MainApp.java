07796d9d474afcbe52af23bdd8295355f1a0d48b
Wojciech <wojt648@gmail.com>
2026-02-05 20:54:08 +0100

    App is now working in JavaFX

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,4 +1,37 @@
 package org.example;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label question = new Label();
+        Label score = new Label();
+        VBox answersContainer = new VBox();
+        answersContainer.setSpacing(10);
+        answersContainer.setAlignment(Pos.CENTER);
+
+        VBox root = new VBox();
+        root.setSpacing(20);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(question,score, answersContainer);
+
+        QuizController quizController = new QuizController(question, answersContainer, score);
+        quizController.startGame();
+
+        Scene scene = new Scene(root, 400, 500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Quiz");
+        primaryStage.show();
+    }
 }
--- src/main/java/org/example/QuizController.java
+++ src/main/java/org/example/QuizController.java
@@ -1,4 +1,69 @@
 package org.example;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.List;
+
 public class QuizController {
+
+    private Quiz quiz;
+    private Label score;
+    private List<IQuestion> questions;
+    private int scoreCounter = 0;
+    private int currentIndex;
+
+    private Label question;
+    private VBox answersContainer;
+
+    public QuizController(Label question, VBox answersContainer, Label score) {
+        this.question = question;
+        this.answersContainer = answersContainer;
+        this.score = score;
+        this.quiz = new Quiz();
+    }
+
+    private void handleAnswer(Answer answer){
+        if (answer.isCorrect()){
+            scoreCounter++;
+            score.setText("Score: " + scoreCounter);
+        }
+        currentIndex++;
+        if (currentIndex >= questions.size()){
+            answersContainer.getChildren().clear();
+            question.setText("Game Finished. Your score: " + scoreCounter);
+        }else{
+            updateView();
+        }
+    }
+
+    private void updateView(){
+        answersContainer.getChildren().clear();
+        IQuestion currentQuestion = questions.get(currentIndex);
+        String text = currentQuestion.getText();
+        question.setText(text);
+
+        for (Answer answer : currentQuestion.getAnswers()) {
+            Button answerButton = new Button(answer.getText());
+            answerButton.setOnAction(event -> {
+                handleAnswer(answer);
+            });
+            answersContainer.getChildren().add(answerButton);
+        }
+    }
+
+    public void startGame(){
+        QuizFileHandler quizFileHandler = new QuizFileHandler();
+        try {
+            questions = quizFileHandler.loadQuestions();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        currentIndex = 0;
+        updateView();
+    }
+
+
 }
